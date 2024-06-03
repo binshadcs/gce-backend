11cc972b0387ca9f33b5156813d36651eabeb61a
Binshad K <binshadcs@gmail.com>
2024-06-04 04:49:39 +0530

    first register endpoint added

--- src/routes/coordinator/index.js
+++ src/routes/coordinator/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { Db } = require('../../config/db');
 const { hashPassword, comparePassword } = require('../util');
+const { CreateGroup } = require('../../types');
 const router = Router()
 
 router.get('/', (req, res)=> {
@@ -16,7 +17,7 @@ router.get('/all', async(req, res)=> {
             res.status(404).json({
                 coordinators
             })
-            Db.end()
+            // Db.end()
         } else {
             res.status(200).json({
                 message : "coordinators not found"
@@ -31,11 +32,86 @@ router.get('/all', async(req, res)=> {
 }) 
 
 router.post('/register', async(req, res)=> {
-    const { categoryId } = req.body;
-    const result = await hashPassword(password)
-    res.status(200).json({
-        message : result
+    const { categoryId,
+            name,
+            location,
+            coordinator_name,
+            whatsapp_number,
+            profession,
+            country,
+            state,
+            district,
+            lsg,
+            username,
+            password
+     } = req.body;
+
+    const result = CreateGroup.safeParse({ categoryId,
+        name,
+        location,
+        coordinator_name,
+        whatsapp_number,
+        profession,
+        country,
+        state,
+        district,
+        lsg,
+        username,
+        password
     })
+    if(result.success) {
+        const hashedPassword = await hashPassword(password)
+        try {
+            let [{insertId}] = await Db.promise().query('INSERT INTO tbl_group_coordinators (gp_id, co_ord_name, co_ord_contact, co_profession, co_username, co_password) VALUES(?, ?, ?, ?, ?, ?)',[-1,
+                coordinator_name,
+                whatsapp_number,
+                profession,
+                username,
+                hashedPassword])
+            // console.log(test)
+            const group_coordinator_id = insertId;
+            try {
+                [{insertId}] = await Db.promise().query('INSERT INTO tbl_group_code (gp_name, gp_code, gp_cat_id, dis_id, lsg_id, gp_coord_id, gp_location, gp_country_id, gp_state_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)',[name,
+                    name,
+                    categoryId,
+                    district,
+                    lsg,
+                    group_coordinator_id,
+                    location,
+                    country,
+                    state
+                    ])
+                const group_id = insertId;
+                try {
+                    [{insertId}] = await Db.promise().query('UPDATE tbl_group_coordinators SET gp_id = ? WHERE co_ord_id = ?',[group_id, group_coordinator_id]); 
+                    res.status(200).json({
+                        group_id,
+                        coordinator_id : group_coordinator_id
+                    })
+                } catch (error) {
+                    console.log(error)
+                    res.status(404).json({
+                        message : "can't insert data"
+                    })
+                }  
+
+            } catch (error) {
+                console.log(error)
+                res.status(404).json({
+                    message : "can't insert data"
+                })
+            } 
+        } catch (error) {
+            console.log(error)
+            res.status(404).json({
+                message : "can't insert data"
+            })
+        } 
+    } else {
+        res.status(400).json({
+            message : "Invalid input"
+        })
+    }
 });
 
 router.post('/login', async(req, res)=> {
--- src/types.js
+++ src/types.js
@@ -1,6 +1,21 @@
 const zod = require('zod');
 
 // Coordinators 
-const CreateCoordinator = zod.object({
-    
+const CreateGroup = zod.object({
+    categoryId : zod.number(),
+    name : zod.string(),
+    location : zod.string(),
+    coordinator_name : zod.string(),
+    whatsapp_number : zod.number(),
+    profession : zod.string(),
+    country : zod.number(),
+    state : zod.number(),
+    district : zod.number(),
+    lsg : zod.number(),
+    username : zod.string(),
+    password : zod.string()
 })
+
+module.exports = {
+    CreateGroup
+}
