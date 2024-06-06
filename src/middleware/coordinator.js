1607e4dafd9a85607ab0207f57f61e477e905ed8
Binshad K <binshadcs@gmail.com>
2024-06-06 12:22:11 +0530

    middle logic changed

--- src/middleware/coordinator.js
+++ src/middleware/coordinator.js
@@ -5,25 +5,33 @@ const jwt = require('jsonwebtoken');
 async function coordinatorAuth(req, res, next) {
     const { cookies } = req;
     const { authorization } = req.headers;
-    const word = authorization.split(" ");
-    // if(cookies.token) {
-    try {
-        // const result = jwt.verify(cookies.token, JwtScret)
-        const result = jwt.verify(word[1], JwtScret)
-        if(result.roleId === 2) {
-            req.userId = result.id;
-            req.groupId = result.groupId;
-            next()
-        } else {
-            res.status(400).json({
-                message : "Try to access with coordinator credential"
+    if(authorization) {
+        let token = await authorization.replace("Bearer ", "");
+        try {
+            // const result = jwt.verify(cookies.token, JwtScret)
+            const result = jwt.verify(token, JwtScret)
+            if(result.roleId === 2) {
+                req.userId = result.id;
+                req.groupId = result.groupId;
+                next()
+            } else {
+                res.status(400).json({
+                    message : "Try to access with coordinator credential"
+                })
+            }
+        } catch (error) {
+            res.status(404).json({
+                message : "token verification failed"
             })
         }
-    } catch (error) {
-        res.status(404).json({
-            message : "token verification failed"
+    } else {
+        res.status(400).json({
+            message : "Authorization header to found"
         })
     }
+    // const word = authorization.split(" ");
+    // if(cookies.token) {
+    
         
     // } else {
     //     res.status(404).json({
--- src/middleware/user.js
+++ src/middleware/user.js
@@ -2,33 +2,40 @@ const { JwtScret } = require("../config/config");
 const jwt = require('jsonwebtoken');
 
 async function userAuth(req, res, next) {
-   const { cookies } = req;
-   const { authorization } = req.headers;
-    const word = authorization.split(" ");
-    // if(cookies.token) {
-        try {
-            // const result = jwt.verify(cookies.token, JwtScret)
-            const result = jwt.verify(word[1], JwtScret)
-            if(result.roleId === 1) {
-               req.userId = result.id
-               req.userGrpId = result.groupId
-               next()
-            } else {
-                res.status(400).json({
-                    message : "Try to access with user credential"
-                })
-            }
-        } catch (error) {
-            res.status(404).json({
-                message : "token verification failed"
-            })
-        }
-        
-    // } else {
-    //     res.status(404).json({
-    //         message : "Invalid token"
-    //     })
-    // } 
-}
+    const { cookies } = req;
+    const { authorization } = req.headers;
+     // const word = authorization.split(" ");
+     if (authorization) {
+     let token = await authorization.replace("Bearer ", "");
+    //  console.log(token);
+     // if(cookies.token) {
+         try {
+             // const result = jwt.verify(cookies.token, JwtScret)
+             const result = jwt.verify(token, JwtScret)
+             if(result.roleId === 1) {
+                req.userId = result.id
+                req.userGrpId = result.groupId
+                next()
+             } else {
+                 res.status(400).json({
+                     message : "Try to access with user credential"
+                 })
+             }
+         } catch (error) {
+             res.status(404).json({
+                 message : "token verification failed"
+             })
+         }
+     } else {
+        res.status(400).json({
+            message : "Authorization header to found"
+        })
+     }
+     // } else {
+     //     res.status(404).json({
+     //         message : "Invalid token"
+     //     })
+     // } 
+ }
 
 module.exports = {userAuth}
