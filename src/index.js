9424afb7a90012d1759a156808c36e54f344fcea
Binshad K <binshadcs@gmail.com>
2024-06-10 22:14:30 +0530

    cors remoeved

--- src/index.js
+++ src/index.js
@@ -8,10 +8,7 @@ const app = express();
 
 app.use(cookieParser())
 app.use(express.json())
-app.use(cors({
-    credentials : true,
-    origin : ["http://localhost:3000", "http://localhost:5173", "https://green-clean-earth.vercel.app/", "https://gce-frontend-kappa.vercel.app/"]
-}));
+app.use(cors()); 
 app.use("/api/v1", mainRouter);
 
 app.get("/status", (req, res) => {
