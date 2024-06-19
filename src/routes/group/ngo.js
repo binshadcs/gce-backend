641271b692ffa33904d9e546c328134348376a3d
Binshad K <binshadcs@gmail.com>
2024-06-19 16:58:44 +0530

    ngo reg updated

--- src/routes/group/ngo.js
+++ src/routes/group/ngo.js
@@ -17,7 +17,7 @@ router.post('/register', async(req, res) => {
             const value = await Db.promise().query('SELECT gp_country_id, gp_state_id, dis_id, lsg_id, gp_city, gp_province FROM tbl_group_code WHERE gp_id = ?', [groupId])
             console.log(value[0][0].gp_country_id)
             try {
-                const [{insertId}] = await Db.promise().query('INSERT INTO tbl_ngo (group_id, no_of_members, country_id, state_id, district_id, lsgd_id, city, province) VALUES(?,?,?,?,?,?, ?, ?)', [groupId, members, value[0][0].gp_country_id, value[0][0].gp_state_id, value[0][0].dis_id, value[0][0].lsg_id], value[0][0].gp_city, value[0][0].gp_province)
+                const [{insertId}] = await Db.promise().query('INSERT INTO tbl_ngo (group_id, no_of_members, country_id, state_id, district_id, lsgd_id, city, province) VALUES(?,?,?,?,?,?, ?, ?)', [groupId, members, value[0][0].gp_country_id, value[0][0].gp_state_id, value[0][0].dis_id, value[0][0].lsg_id, value[0][0].gp_city, value[0][0].gp_province])
                 
                 res.status(200).json({
                     NgoId : insertId,
